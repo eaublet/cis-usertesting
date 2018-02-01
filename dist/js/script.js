2efb404c9c2f6e23ef1324ff8591fe3eaa0e3690
Émile Aublet <eaublet@aldogroup.com>
2018-01-31 23:57:05 -0500

    Added few styles and functions on PDP

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,12 +1,64 @@
 (function() {
-  var log;
+  var initBuySticky, initNav, initPDP, log, sizeSelector;
 
-  log = function(log) {
-    return console.log(log);
+  log = function(msg) {
+    return console.log(msg);
+  };
+
+  initBuySticky = function() {
+    var buySticky, elemTop, hideSticky, pageTop, showSticky, targetIn;
+    buySticky = $('.buySticky');
+    targetIn = ".addToCart";
+    pageTop = $(document).scrollTop();
+    elemTop = $(targetIn).offset().top;
+    showSticky = function() {
+      if ($(document).scrollTop() >= $(targetIn).offset().top) {
+        return buySticky.addClass('visible');
+      }
+    };
+    hideSticky = function() {
+      return buySticky.removeClass('visible');
+    };
+    if (elemTop <= pageTop) {
+      showSticky();
+    }
+    if (buySticky.length) {
+      return inView(targetIn).on('enter', function(el) {
+        return hideSticky();
+      }).on('exit', function(el) {
+        showSticky();
+      });
+    }
+  };
+
+  initNav = function() {
+    $('nav.mobile').headroom({
+      tolerance: {
+        up: 5,
+        down: 10
+      },
+      offset: ($('.surnav.mobile').outerHeight() || 0) + $('nav.mobile').outerHeight()
+    });
+    initBuySticky();
+  };
+
+  sizeSelector = function() {
+    return $('.sizeList li').click(function() {
+      $('.sizeList li').each(function() {
+        return $(this).removeClass('active');
+      });
+      $(this).addClass('active');
+      return $('.sizeSelected').html($(this).text());
+    });
+  };
+
+  initPDP = function() {
+    return sizeSelector();
   };
 
   $(document).ready(function() {
-    return log('toto');
+    initNav();
+    return initPDP();
   });
 
 }).call(this);
--- src/coffee/script.coffee
+++ src/coffee/script.coffee
@@ -1,5 +1,48 @@
-log = (log) ->
-	console.log(log)
+log = (msg) ->
+	console.log(msg)
+
+initBuySticky = () ->
+	buySticky = $('.buySticky')
+	targetIn = ".addToCart"
+	pageTop = $(document).scrollTop()
+	elemTop = $(targetIn).offset().top
+
+	showSticky = () ->
+		if $(document).scrollTop() >= $(targetIn).offset().top
+			buySticky.addClass 'visible'
+	hideSticky = () ->
+		buySticky.removeClass 'visible'
+	
+	if elemTop <= pageTop
+		showSticky()
+
+	if buySticky.length
+		inView(targetIn).on('enter', (el) ->
+			hideSticky()
+		).on 'exit', (el) ->
+			showSticky()
+			return
+
+initNav = () ->
+	$('nav.mobile').headroom
+		tolerance:
+			up: 5
+			down: 10
+		offset: ($('.surnav.mobile').outerHeight() || 0 ) + $('nav.mobile').outerHeight()
+	initBuySticky()
+	return
+
+
+sizeSelector = () ->
+	$('.sizeList li').click ->
+		$('.sizeList li').each ->
+			$(@).removeClass 'active'
+		$(@).addClass 'active'
+		$('.sizeSelected').html($(@).text())
+ 
+initPDP = () ->
+	sizeSelector()
 
 $(document).ready ->
-	log 'toto'
+	initNav()
+	initPDP()
