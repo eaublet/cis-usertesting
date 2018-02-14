a12f6b89a86121fe1b2c3f202504f64ff122f5cf
Badi Labassi <blabassi@aldogroup.com>
2018-02-14 15:53:56 -0500

    Add ServiceWorker PreCache (#1)

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,23 @@
 /*global module:false*/
-module.exports = function(grunt) {
 
+'use strict';
+
+var packageJson = require('./package.json');
+var path = require('path');
+var swPrecache = require('sw-precache');
+
+module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     clean: {
-      html: "dist/**/*.html"
+      html: 'dist/**/*.html'
+    },
+    swPrecache: {
+      dev: {
+        handleFetch: false,
+        rootDir: 'dist'
+      }
     },
     pug: {
       compile: {
@@ -14,13 +26,15 @@ module.exports = function(grunt) {
             debug: false
           }
         },
-        files: [ {
-          cwd: "src/pug",
-          src: ['**/*.pug', '!**/blocks/**'],
-          dest: "dist/",
-          expand: true,
-          ext: ".html"
-        } ]
+        files: [
+          {
+            cwd: 'src/pug',
+            src: ['**/*.pug', '!**/blocks/**'],
+            dest: 'dist/',
+            expand: true,
+            ext: '.html'
+          }
+        ]
       }
     },
     sass: {
@@ -29,28 +43,30 @@ module.exports = function(grunt) {
           style: 'compact',
           sourcemap: 'inline'
         },
-        files:
-          [{
+        files: [
+          {
             expand: true,
             cwd: 'src/scss',
             src: ['*.scss', '!**/mixins/**', '!**/parts/**'],
             dest: 'dist/css/',
             ext: '.css'
-          }]
+          }
+        ]
       },
       prod: {
         options: {
-          style: "compressed",
+          style: 'compressed',
           sourcemap: 'none'
         },
-        files:
-          [{
+        files: [
+          {
             expand: true,
             cwd: 'src/scss',
             src: ['*.scss'],
             dest: 'dist/css/',
             ext: '.css'
-          }]
+          }
+        ]
       }
     },
     autoprefixer: {
@@ -75,8 +91,8 @@ module.exports = function(grunt) {
       files: ['src/**/*.*'],
       tasks: ['neweronly'],
       options: {
-        livereload: true,
-      },
+        livereload: true
+      }
     },
     connect: {
       server: {
@@ -91,11 +107,60 @@ module.exports = function(grunt) {
     }
   });
 
+  function writeServiceWorkerFile(rootDir, handleFetch, callback) {
+    var config = {
+      cacheId: packageJson.name,
+      // dynamicUrlToDependencies: {
+      //   'dynamic/page1': [
+      //     path.join(rootDir, 'views', 'layout.jade'),
+      //     path.join(rootDir, 'views', 'page1.jade')
+      //   ],
+      //   'dynamic/page2': [
+      //     path.join(rootDir, 'views', 'layout.jade'),
+      //     path.join(rootDir, 'views', 'page2.jade')
+      //   ]
+      // },
+      // If handleFetch is false (i.e. because this is called from swPrecache:dev), then
+      // the service worker will precache resources but won't actually serve them.
+      // This allows you to test precaching behavior without worry about the cache preventing your
+      // local changes from being picked up during the development cycle.
+      handleFetch: handleFetch,
+      logger: grunt.log.writeln,
+      staticFileGlobs: [
+        rootDir + '/css/**.*',
+        rootDir + '/fonts/**/**.*',
+        rootDir + '/**/**.html',
+        rootDir + '/img/**/**.*',
+        rootDir + '/js/**/**.*'
+      ],
+      stripPrefix: rootDir + '/',
+      // verbose defaults to false, but for the purposes of this demo, log more.
+      verbose: true
+    };
+
+    swPrecache.write(path.join(rootDir, 'service-worker.js'), config, callback);
+  }
+
+  grunt.registerMultiTask('swPrecache', function() {
+    /* eslint-disable no-invalid-this */
+    var done = this.async();
+    var rootDir = this.data.rootDir;
+    var handleFetch = this.data.handleFetch;
+    /* eslint-enable */
+
+    writeServiceWorkerFile(rootDir, handleFetch, function(error) {
+      if (error) {
+        grunt.fail.warn(error);
+      }
+      done();
+    });
+  });
+
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-pug');
   grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks("grunt-reload");
+  grunt.loadNpmTasks('grunt-reload');
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-coffee');
   grunt.loadNpmTasks('grunt-contrib-sass');
@@ -103,12 +168,29 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-newer');
 
   // Default task.
-  grunt.registerTask('default', ['sass:dev', 'autoprefixer', 'pug', 'coffee']);
-  grunt.registerTask('neweronly', ['sass:dev', 'autoprefixer', 'pug', 'newer:coffee']);
-  grunt.registerTask('prod', ['sass:prod', 'autoprefixer', 'pug', 'coffee']);
+  grunt.registerTask('default', [
+    'sass:dev',
+    'autoprefixer',
+    'swPrecache',
+    'pug',
+    'coffee'
+  ]);
+  grunt.registerTask('neweronly', [
+    'sass:dev',
+    'autoprefixer',
+    'swPrecache',
+    'pug',
+    'newer:coffee'
+  ]);
+  grunt.registerTask('prod', [
+    'sass:prod',
+    'autoprefixer',
+    'swPrecache',
+    'pug',
+    'coffee'
+  ]);
 
   // USE THESE TASKS
   grunt.registerTask('build', ['clean:html', 'prod']);
-  grunt.registerTask('serve', ['default', 'connect:server', 'watch'])
-
+  grunt.registerTask('serve', ['default', 'connect:server', 'watch']);
 };
